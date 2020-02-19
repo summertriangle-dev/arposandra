6c3e976a596e1013b1b9b0f7d4e7c96a414501fb
karin <summertriangle.dev@gmail.com>
2020-02-18 18:59:00 -0800

    add the cave slime experiment

--- frontend/js/experiments.js
+++ frontend/js/experiments.js
@@ -1,10 +1,49 @@
 import {ModalManager} from "./modals"
 import React from "react"
 
+// Do not localize anything in this file.
+
+export const FLG_CS_AR_E = 0x1
+
+function _validateCaveSlimeEntry(password) {
+    let flags = localStorage.getItem("as$caveSlimeFlags")
+    if (flags === null) {
+        flags = 0
+    } else {
+        flags = parseInt(flags)
+    }
+
+    if (password === "11162029") {
+        let msg
+        if (flags & FLG_CS_AR_E) {
+            msg = "It sounds like the mechanism has returned to its normal position."
+        } else {
+            msg = "It sounds like something heavy moved from deeper inside the cave!"
+        }
+
+        flags ^= FLG_CS_AR_E
+
+        ModalManager.pushModal((dismiss) => {
+            return <section className="modal-body tlinject-modal">
+                <p>{msg}</p>
+                <div className="form-row kars-fieldset-naturalorder">
+                    <button className="item btn btn-primary" onClick={dismiss}>Dismiss</button>
+                </div>
+            </section>
+        })
+    } 
+
+    localStorage.setItem("as$caveSlimeFlags", flags.toString())
+}
+
 export function injectIntoPage() {
     document.querySelector("#uilib-modal-button").addEventListener("click", () => {
         ModalManager.pushModal(() => {
             return <p>hello!</p>
         })
+    }, {passive: true})
+
+    document.querySelector("#cs-submit-button").addEventListener("click", () => {
+        _validateCaveSlimeEntry(document.querySelector("#cs-input").value)
     }, {passive: true})
 }
