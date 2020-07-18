52f8f331b1dc2a065726355cea5b08a30edc07d1
karin <summertriangle.dev@gmail.com>
2020-07-17 21:24:26 -0700

    fix shellcheck complaints

--- maintenance/start.sh
+++ maintenance/start.sh
@@ -5,45 +5,45 @@ executable=$1; shift
 
 case $executable in
     news)
-        exec python3 news/get_news.py $@
+        exec python3 news/get_news.py "$@"
         ;;
     reparse)
-        exec python3 news/reparse_news.py $@
+        exec python3 news/reparse_news.py "$@"
         ;;
     sync-master) # ... sync-master [rgn] [karstool args...]
         SERVER=$1; shift
-        exec python3 -m astool ${SERVER} --quiet dl_master
+        exec python3 -m astool "${SERVER}" --quiet dl_master
         ;;
     sync-cache)
         SERVER=$1; shift
-        exec python3 -m astool ${SERVER} --quiet pkg_sync main card:%
+        exec python3 -m astool "${SERVER}" --quiet pkg_sync main card:%
         ;;
     sync-cache-full)
         SERVER=$1; shift
-        exec python3 -m astool ${SERVER} --quiet pkg_sync everything
+        exec python3 -m astool "${SERVER}" --quiet pkg_sync everything
         ;;
     sync-both)
         SERVER=$1; shift
-        python3 -m astool ${SERVER} --quiet dl_master && \
-            python3 -m astool ${SERVER} --quiet pkg_sync main card:%
+        python3 -m astool "${SERVER}" --quiet dl_master && \
+            python3 -m astool "${SERVER}" --quiet pkg_sync main card:%
         ;;
     astool_command)
-        exec python3 -m astool $@
+        exec python3 -m astool "$@"
         ;;
     mtrack)
-        exec python3 mtrack/mtrack.py $@
+        exec python3 mtrack/mtrack.py "$@"
         ;;
     border-ingest)
         SERVER=$1; shift
-        exec python3 border/border.py $SERVER $@
+        exec python3 border/border.py "$SERVER" "$@"
         ;;
     border-ingest2)
-        exec python3 border/et_fake.py $@
+        exec python3 border/et_fake.py "$@"
         ;;
     none)
         exit 0
         ;;
     *)
-        echo "Unrecognized command: $@."
+        echo "Unrecognized command: $1 $*."
         exit 1
 esac
