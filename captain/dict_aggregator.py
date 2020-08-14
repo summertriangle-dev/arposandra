70f8ed3120eff8fa4ad26ba7908f07a87f83e15c
karin <summertriangle.dev@gmail.com>
2020-08-14 00:19:12 -0700

    remove dummy values from dict translations

--- captain/dict_aggregator.py
+++ captain/dict_aggregator.py
@@ -1,14 +1,42 @@
 from collections import namedtuple
+from dataclasses import dataclass
+from typing import Dict, Iterable, Optional, Tuple, Set
 
-Alternative = namedtuple("Alternative", ("name", "code", "access"))
+from libcard2.string_mgr import DictionaryAccess
+
+# Note: do not edit - this file will be removed after maintenance
+# refactor + auto tli inserter is merged
+
+TStrings = Dict[str, str]
+TAltKeys = Set[str]
+
+
+@dataclass
+class Alternative(object):
+    name: str
+    code: str
+    access: DictionaryAccess
 
 
 class DictionaryAggregator(object):
-    def __init__(self, master_dict, choices):
+    def __init__(self, master_dict: DictionaryAccess, choices: Dict[str, Alternative]):
         self.master = master_dict
         self.choices = choices
 
-    def lookup_strings(self, stringset, preferred_language=None):
+    def filter_strings(self, stringsd: TStrings):
+        to_remove = set()
+        for key, value in stringsd.items():
+            if "仮" in value:
+                to_remove.add(key)
+
+        for k in to_remove:
+            stringsd.pop(k)
+
+        return stringsd
+
+    def lookup_strings(
+        self, stringset: Iterable[str], preferred_language: Optional[str] = None
+    ) -> Tuple[TStrings, TAltKeys]:
         ss = set(stringset)
 
         alt = self.choices.get(preferred_language)
@@ -16,8 +44,10 @@ class DictionaryAggregator(object):
             return self.master.lookup_strings(ss), set()
 
         stage1 = alt.access.lookup_strings(ss)
+        stage1 = self.filter_strings(stage1)
+
         alt_set = set(stage1.keys())
-        stage2 = self.master.lookup_strings(ss - set(stage1.keys()))
+        stage2 = self.master.lookup_strings(ss - alt_set)
         stage2.update(stage1)
 
         return stage2, alt_set
