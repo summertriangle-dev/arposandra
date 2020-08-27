952a269a3b9921f5f98ee684536435c4e765e75c
karin <summertriangle.dev@gmail.com>
2020-08-27 14:07:19 -0700

    try using Preact for prod builds
    normal react is still used for dev tools, in dev mode

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,18 @@
 const webpack = require("webpack")
 const process = require("process")
 
+const resolve = () => {
+    if (process.env.NODE_ENV === "development") {
+        return {}
+    }
+
+    return {alias: {
+        "react": "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+    }}
+}
+
 module.exports = {
     mode: "production",
     entry: {
@@ -14,32 +26,35 @@ module.exports = {
         publicPath: process.env["WDS_PUBLIC_PATH"] || "/static/js/",
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/],
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/react"],
-                        plugins: ["@babel/plugin-syntax-dynamic-import"]
-                    }
+        rules: [{
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/react"],
+                    plugins: ["@babel/plugin-syntax-dynamic-import"]
                 }
-            },
-        ]
+            }
+        }, ]
     },
     devtool: "source-map",
     devServer: {
         compress: true,
         host: "0.0.0.0",
         port: 5002,
-        headers: {"Access-Control-Allow-Origin": "*"},
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
         disableHostCheck: true,
     },
     optimization: {
-        splitChunks: { chunks: "all" }
+        splitChunks: {
+            chunks: "all"
+        }
     },
     plugins: [
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|ja/)
-    ]
+    ],
+    resolve: resolve()
 }
