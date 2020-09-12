d6733b339f6aff9c4df6ba2fc5d8f60e25a11f01
karin <summertriangle.dev@gmail.com>
2020-09-12 16:58:48 -0700

    add sidebar search form

--- frontend/js/gutter.js
+++ frontend/js/gutter.js
@@ -1,11 +1,11 @@
 
 function _openGutterAction(event) {
     console.debug("_openGutterAction trigger!")
-    const h = document.querySelector(".gutter-menu-host")
-    const animationTarget = h.querySelector(".gutter-menu-main")
+    const host = document.querySelector(".gutter-host")
+    const animationTarget = host.querySelector(".gutter")
 
-    if (!h.classList.contains("opened")) {
-        h.classList.add("opened")
+    if (!host.classList.contains("opened")) {
+        host.classList.add("opened")
         console.debug("starting anim")
         animationTarget.classList.add("animation-state-start")
         requestAnimationFrame(() => {
@@ -14,25 +14,27 @@ function _openGutterAction(event) {
     } else {
         animationTarget.classList.add("animation-state-start")
         animationTarget.addEventListener("transitionend", () => {
-            h.classList.remove("opened")
+            host.classList.remove("opened")
         }, {once: true})
     }
 }
 
 function _dismissGutterAction(event) {
-    const host = document.querySelector(".gutter-menu-host")
+    const host = document.querySelector(".split-view .secondary")
     if (event.currentTarget === event.target && host.classList.contains("opened")) {
         _openGutterAction(event)
     }
 }
 
 export function injectIntoPage() {
-    const triggers = document.querySelectorAll("#open-gutter")
+    const triggers = document.querySelectorAll(".open-gutter")
     for (let i = 0; i < triggers.length; i++) {
         const button = triggers[i]
         button.addEventListener("click", _openGutterAction, {passive: true})
     }
 
-    const host = document.querySelector(".gutter-menu-host")
-    host.addEventListener("click", _dismissGutterAction, {passive: true})
+    const host = document.querySelector(".gutter-host")
+    if (host) {
+        host.addEventListener("click", _dismissGutterAction, {passive: true})
+    }
 }
