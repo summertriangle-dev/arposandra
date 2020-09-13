466e5b2dd83c3e29ef3e6a610532fda4454cb041
karin <summertriangle.dev@gmail.com>
2020-09-13 14:42:44 -0700

    fix gutter overflow behaviour

--- frontend/js/gutter.js
+++ frontend/js/gutter.js
@@ -1,5 +1,40 @@
+const GUTTER_MIN_OFFSET_FROM_BOTTOM = 30
 
-function _openGutterAction(event) {
+const adjustGutterScrollable = (() => {
+    const mem = {scrollEnabled: true, chromeSize: undefined, maxHeight: false}
+
+    return (gutter) => {
+        if (window.innerWidth >= 1560 && mem.chromeSize === undefined) {
+            mem.chromeSize = gutter.getBoundingClientRect().top + GUTTER_MIN_OFFSET_FROM_BOTTOM
+        }
+
+        if (window.innerWidth >= 1560) {
+            gutter.style.maxHeight = (window.innerHeight - mem.chromeSize) + "px"
+            gutter.style.overflowY = "scroll"
+            mem.scrollEnabled = true
+            mem.maxHeight = true
+        } else {
+            if (mem.maxHeight) {
+                gutter.style.maxHeight = null
+                mem.maxHeight = false
+            }
+        }
+
+        if (!mem.scrollEnabled) {
+            if (gutter.clientHeight < gutter.scrollHeight) {
+                gutter.style.overflowY = "scroll"
+                mem.scrollEnabled = true
+            }
+        } else {
+            if (gutter.clientHeight >= gutter.scrollHeight) {
+                gutter.style.overflowY = "hidden"
+                mem.scrollEnabled = false
+            }
+        }
+    }
+})()
+
+function _openGutterAction() {
     console.debug("_openGutterAction trigger!")
     const host = document.querySelector(".gutter-host")
     const animationTarget = host.querySelector(".gutter")
@@ -17,12 +52,14 @@ function _openGutterAction(event) {
             host.classList.remove("opened")
         }, {once: true})
     }
+
+    adjustGutterScrollable(animationTarget)
 }
 
 function _dismissGutterAction(event) {
-    const host = document.querySelector(".split-view .secondary")
+    const host = document.querySelector(".gutter-host")
     if (event.currentTarget === event.target && host.classList.contains("opened")) {
-        _openGutterAction(event)
+        _openGutterAction()
     }
 }
 
@@ -36,5 +73,11 @@ export function injectIntoPage() {
     const host = document.querySelector(".gutter-host")
     if (host) {
         host.addEventListener("click", _dismissGutterAction, {passive: true})
+    }
+
+    const gutterScrollable = document.querySelector(".gutter")
+    if (gutterScrollable) {
+        window.addEventListener("resize", () => adjustGutterScrollable(gutterScrollable), {passive: true})
+        adjustGutterScrollable(gutterScrollable)
     }
 }
