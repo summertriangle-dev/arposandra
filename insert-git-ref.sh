4556eb9798f6857474b96e2615348171d413ce7b
karin <summertriangle.dev@gmail.com>
2020-11-24 00:08:50 -0800

    quote git hash in yml

--- insert-git-ref.sh
+++ insert-git-ref.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 REF=$(git describe --always)
-sed -i'' -E "s|: (.+) # @exp_as_git_rev@|: $REF # @exp_as_git_rev@|" docker-compose.yml \
+sed -i'' -E "s|: (.+) # @exp_as_git_rev@|: \"$REF\" # @exp_as_git_rev@|" docker-compose.yml \
     && ( rm -f docker-compose.yml.next || true )
