0a6a7b528f61d58770381fb68b6627d3db12d952
karin <summertriangle.dev@gmail.com>
2021-06-18 18:06:06 -0700

    temporary fix for card 477 skill name

--- captain/dict_aggregator.py
+++ captain/dict_aggregator.py
@@ -4,8 +4,6 @@ from typing import Dict, Iterable, Optional, Tuple, Set
 
 from libcard2.string_mgr import DictionaryAccess
 
-# Note: do not edit - this file will be removed after maintenance
-# refactor + auto tli inserter is merged
 
 TStrings = Dict[str, str]
 TAltKeys = Set[str]
@@ -24,10 +22,15 @@ class DictionaryAggregator(object):
         self.choices = choices
 
     def filter_strings(self, stringsd: TStrings):
-        to_remove = set()
+        to_remove = []
+
+        # to be removed, hopefully when the sync happens
         for key, value in stringsd.items():
             if "仮" in value:
-                to_remove.add(key)
+                to_remove.append(key)
+
+            if key == "k.passive_skill_name_321030111":
+                to_remove.append(key)
 
         for k in to_remove:
             stringsd.pop(k)
@@ -39,7 +42,7 @@ class DictionaryAggregator(object):
     ) -> Tuple[TStrings, TAltKeys]:
         ss = set(stringset)
 
-        alt = self.choices.get(preferred_language)
+        alt = self.choices.get(preferred_language)  # type: ignore
         if not alt:
             return self.master.lookup_strings(ss), set()
 
